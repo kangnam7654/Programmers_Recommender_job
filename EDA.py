ce6fa6c0cccaaf448529781a27b2a92c943321d8
Kangnam Kim <kangnam7654@naver.com>
2022-01-19 22:42:27 +0900

    EDA edit

--- EDA.py
+++ EDA.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 train_file = pd.read_csv('./data/train.csv')
+job_tag = pd.read_csv('./data/job_tags.csv')
+
+a = train_file.copy()
+b = pd.merge(a, job_tag, on='jobID', how='outer')
 
 user = set(train_file['userID'].tolist())
 job = set(train_file['jobID'].tolist())
